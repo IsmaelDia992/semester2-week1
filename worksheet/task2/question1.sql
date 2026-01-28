274301584213c5ae7d3faae4abf040e56fc6cb53
IsmaelDia992 <vfhv0117@leeds.ac.uk>
2026-01-28 12:12:45 +0000

    1 and 2 i think done

--- worksheet/task2/question1.sql
+++ worksheet/task2/question1.sql
@@ -1,3 +1,5 @@
 -- List all students who enrolled in the year 2022.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, EnrolmentYear
+
+SELECT StudentID, FirstName, LastName FROM Student WHERE EnrolmentYear = 2022;
